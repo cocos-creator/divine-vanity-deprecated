b8bdbf807d5c00f9507778473c47ffae08291f57
Nan Wang <nantas@gmail.com>
2016-01-30 14:12:08 +0800

    add audio clips. 『』

---
+++ assets/scripts/controllers/AudioMng.js
@@ -0,0 +1,20 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        bgm: {
+            default: null,
+            url: cc.AudioClip
+        }
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
