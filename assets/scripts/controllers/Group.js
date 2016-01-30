f6d7efd6316b40ced995bc5ac7f78435b0b36294
pandamicro <lphuabin@gmail.com>
2016-01-30 21:36:45 +0800

    Fix bugs

--- assets/scripts/controllers/Group.js
+++ assets/scripts/controllers/Group.js
@@ -58,7 +58,9 @@ cc.js.mixin(Group.prototype, {
 
     split: function (wish, learningGroup) {
         if (this.canLearn(wish)) {
-            var leftCount = this.people.length - wish.ritualNeed;
+            var need = wish.ritualNeed + Math.ceil(Math.random() * 2);
+            var leftCount = this.people.length - need;
+            leftCount < 0 && (leftCount = 0);
             for (var i = this.people.length-1; i >= leftCount; --i) {
                 learningGroup.addMember(this.people[i]);
             }
@@ -86,30 +88,42 @@ cc.js.mixin(Group.prototype, {
             case States.WORSHINPING:
                 // Not learning anymore
                 this.poses.length = 0;
+                this.wish = null;
                 var people = this.people;
                 var society = this.society;
-                people.forEach((person, index) => {
-                    var behavior = person.getComponent('HumanBehavior');
-                    var lostCount = 0;
-                    // Correct pose
-                    if (detail === behavior.currentPose) {
-                        behavior.currentState = States.WORSHINPING;
-                    }
-                    // Incorrect
-                    else {
-                        if (Math.random() < society.lostCoef) {
-                            behavior.currentState = States.LOST;
-                            delete people[index];
-                            lostCount++;
+                this.society.scheduleOnce(function () {
+                    people.forEach((person, index) => {
+                        var behavior = person.getComponent('HumanBehavior');
+                        var lostCount = 0;
+                        // Correct pose
+                        if (detail === behavior.currentPose) {
+                            behavior.currentState = States.WORSHIPING;
                         }
-                    }
-                    society.lost(lostCount);
-                });
-                this.wish = null;
+                        // Incorrect
+                        else {
+                            if (Math.random() < society.lostCoef) {
+                                behavior.currentState = States.LOST;
+                                delete people[index];
+                                lostCount++;
+                            }
+                            else {
+                                behavior.currentState = States.DEFAULT;
+                            }
+                        }
+                        society.lost(lostCount);
+                    });
+                }, 0);
                 this.countdown = 3;
                 break;
             }
         }
+    },
+
+    toDefault: function () {
+        this.people.forEach((person) => {
+            var behavior = person.getComponent('HumanBehavior');
+            behavior.currentState = States.DEFAULT;
+        });
     },
     
     update: function (dt) {
@@ -134,10 +148,7 @@ cc.js.mixin(Group.prototype, {
                 this.countdown = this.wish.poseDuration;
                 break;
             case States.WORSHINPING:
-                this.people.forEach((person) => {
-                    var behavior = person.getComponent('HumanBehavior');
-                    behavior.currentState = States.DEFAULT;
-                });
+                this.society.scheduleOnce(this.toDefault.bind(this), 0);
                 this.state = States.DEFAULT;
                 this.society.rejointDefault(this);
                 break;
