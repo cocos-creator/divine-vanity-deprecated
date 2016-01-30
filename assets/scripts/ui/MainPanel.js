f1f9ea096bb0a6b71dc15735874ee0440738b45f
knoxHuang <821686174@qq.com>
2016-01-30 23:28:36 +0800

    Update MainPanel and PowerBar

--- assets/scripts/ui/MainPanel.js
+++ assets/scripts/ui/MainPanel.js
@@ -32,6 +32,24 @@ var MainPanel = cc.Class ({
 
     start: function () {
         this.instantiate();
+        this.unlockBtn(0);
+
+        this.state = 0;
+        var self = this;
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed: function(keyCode, event) {
+                switch(keyCode) {
+                    case cc.KEY.b:
+                        self.state++;
+                        if (self.state > 2) {
+                            self.state = 0;
+                        }
+                        self.unlockBtn(self.state);
+                        break;
+                }
+            }
+        }, this.node);
     },
 
     useSkill: function (skillID, costValue) {
@@ -43,10 +61,30 @@ var MainPanel = cc.Class ({
         AudioMng.instance.playWishSFX(skillID);
     },
 
+    // 按钮状态 是否禁用
     chackBtnState: function (resources) {
         for (let i = 0, len = this.skills.length; i < len; ++i) {
             var skill = this.skills[i];
             skill.button.interactable = skill.cost <= resources.instance.curPower;
+        }
+    },
+
+    unlockBtn: function (state) {
+        for (let i = 0, len = this.skills.length; i < len; ++i) {
+            this.skills[i].node.active = false;
+        }
+
+        var firstPosX = 0;
+        for (let i = 0; i <= state; i++) {
+            var skill = this.skills[i];
+            if (i === 0) {
+                firstPosX = 200 - (state * 100);
+                skill.node.x = firstPosX;
+            }
+            else {
+                skill.node.x = firstPosX + (100 * i);
+            }
+            skill.node.active = true;
         }
     },
 
