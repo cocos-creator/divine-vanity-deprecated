3a65dc1c6ab345732b80d7beb7d3dfd16a3e54ed
knoxHuang <821686174@qq.com>
2016-01-31 14:08:32 +0800

    add MainPanel Anim

--- assets/scripts/ui/MainPanel.js
+++ assets/scripts/ui/MainPanel.js
@@ -1,15 +1,61 @@
+
+var Society = require('Society');
+
 var MainPanel = cc.Class({
     extends: cc.Component,
 
     properties: {
+        society: {
+            default: null,
+            type: Society
+        },
+        animCtrl: {
+            default: null,
+            type: cc.Animation
+        },
+        eyesNode: {
+            default: null,
+            type: cc.Node
+        },
+        startGameBtn: {
+            default: null,
+            type: cc.Animation
+        }
 
     },
 
     onLoad: function () {
-
+        this.society.pause();
+        this.hasMoveScene = false;
+        var scene = cc.director.getScene();
+        scene.y = -1450;
     },
 
-    runGameClick: function () {
-        cc.director.loadScene("game");
+    hideMainPanelAnimEnd: function () {
+        this.society.resume();
+    },
+
+    runMoveScene: function () {
+        var state = this.startGameBtn.getAnimationState('fadeInStartBtn');
+        state.wrapMode = cc.WrapMode.Reverse;
+        this.startGameBtn.play('fadeInStartBtn');
+        this.hasMoveScene = true;
+    },
+
+    update: function (dt) {
+        if (this.hasMoveScene) {
+            var scene = cc.director.getScene();
+            if (scene.y >= 0) {
+                return;
+            }
+            scene.y += 300 * dt;
+            this.eyesNode.y -= 300 * dt;
+            if (scene.y > 0) {
+                scene.y = 0;
+                this.hasMoveScene = false;
+                this.animCtrl.play('hideMainPanel');
+            }
+        }
     }
+
 });
