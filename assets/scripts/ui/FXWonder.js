618653e56ac81902e193863ea0ce5550c48d3506
Nan Wang <nantas@gmail.com>
2016-01-31 14:54:47 +0800

    fixed wonder pop.

--- assets/scripts/ui/FXWonder.js
+++ assets/scripts/ui/FXWonder.js
@@ -4,14 +4,33 @@ cc.Class({
     properties: {
         particle: cc.ParticleSystem,
         anim: cc.Animation,
-        sfWonders: [cc.SpriteFrame]
     },
 
     playAnim (idx) {
-        let sprite = this.anim.getComponent(cc.Sprite);
-        sprite.spriteFrame = this.sfWonders[idx];
-        sprite.enabled = true;
-        this.anim.play('pop');
+        switch (idx) {
+            case 0:
+                this.anim.play('pop');
+                this.node.y -= cc.random0To1() * 80;
+                this.node.scale *= cc.random0To1() * 0.3 + 1;
+                break;
+            case 1:
+                this.anim.play('empty');
+                this.node.y += cc.random0To1() * 100;
+                break;
+            case 2:
+                this.anim.play('pop_maya');
+                this.node.y += cc.random0To1() * 60;
+                this.node.scale *= cc.random0To1() * 0.3 + 1;
+                break;
+            case 3:
+                this.anim.play('pop_egypt');
+                this.node.y += cc.random0To1() * 80;
+                break;
+            case 4:
+                this.anim.play('pop');
+                break;
+        }
+        // this.anim.play('pop');
         this.particle.resetSystem();
     }
 
